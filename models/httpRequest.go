a760cb9643b22cc3958500c1453a3421a9ffdc17
Vishnu Narayanan <iamwishnu@gmail.com>
2017-12-07 05:25:51 +0530

    temp commit - user validation

--- models/httpRequest.go
+++ models/httpRequest.go
@@ -7,3 +7,8 @@ type SignUpReq struct {
 	LastName  string `json:"lastName"`
 	Password  string `json:"password"`
 }
+
+type LoginReq struct{
+	Email string `json:"emailId"`
+	Password string `json:"password"`
+}
